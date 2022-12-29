72da63ff0e47fe5d582781956c02174d23bd8ed6
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2022-12-29 11:19:09 +0100

    Update 3_Multi_input.py

--- pages/3_Multi_input.py
+++ pages/3_Multi_input.py
@@ -65,7 +65,7 @@ for k in list_tables:
 	df=input_DB[k]
 	st.write(k)
 	# Add new row to DataFrame
-	new_row = ['hola']
+	new_row = ['']
 	df = df.append(new_row, ignore_index=True)
 	
 	st.table(df)
