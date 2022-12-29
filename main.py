d01a34e3cee5f4efe3406658a0ec7a4f66bc661e
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2022-12-29 16:03:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,27 +12,18 @@ import base64
 
 st.write("***THIS IS THE MAIN PAGE***")
 
-import PyPDF2
 
-# Read the PDF file
-with open('Input_files/carsecn.pdf', 'rb') as f:
-    pdf = PyPDF2.PdfFileReader(f)
 
-# Display the PDF contents
-for page in pdf.pages:
-    st.write(page.extractText())
+def show_pdf(file_path):
+    with open(file_path,"rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-#def show_pdf(file_path):
-#    with open(file_path,"rb") as f:
-#        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
- #   pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="1000" type="application/pdf"></iframe>'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="1000" type="application/pdf"></iframe>'
     
-  #  st.markdown(pdf_display, unsafe_allow_html=True)
+    st.markdown(pdf_display, unsafe_allow_html=False)
+    
+    #st.write("The original pdf")
     
     
-#st.write("The original pdf")
-    
-    
-#show_pdf("Input_files/carsecn.pdf")
+show_pdf("Input_files/carsecn.pdf")
 
