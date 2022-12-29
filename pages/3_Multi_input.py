416117ebc676214cc131d54aedd0ea68a5906f3a
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2022-12-29 15:48:47 +0100

    Update 3_Multi_input.py

--- pages/3_Multi_input.py
+++ pages/3_Multi_input.py
@@ -70,29 +70,7 @@ for k in list_tables:
 		st.session_state.data=pd.DataFrame(data, columns=data.columns)
 		#st.dataframe(df)
 		AgGrid(data)
-		gb = GridOptionsBuilder.from_dataframe(data)
-		gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
-		gb.configure_side_bar() #Add a sidebar
-		gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
-		gridOptions = gb.build()
-
-		grid_response = AgGrid(
-		    data,
-		    gridOptions=gridOptions,
-		    data_return_mode='AS_INPUT', 
-		    update_mode='MODEL_CHANGED', 
-		    fit_columns_on_grid_load=False,
-		    theme='blue', #Add theme color to the table
-		    enable_enterprise_modules=True,
-		    height=350, 
-		    width='100%',
-		    reload_data=True
-		)
-
-		data = grid_response['data']
-		selected = grid_response['selected_rows'] 
-		df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
-	
+		in
 		
 		# Add a clear button to clear the table data
 		#if st.button("Clear table - "+str(k), key='unique_button_key' +str(k)):
