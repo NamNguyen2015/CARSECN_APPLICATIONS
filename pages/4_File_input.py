8aa806a90bb3faec7597d6656a6c657071b30ab6
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-01-12 10:46:30 +0100

    Update 4_File_input.py

--- pages/4_File_input.py
+++ pages/4_File_input.py
@@ -45,7 +45,9 @@ path1=os.getcwd()
 
 st.write(path1)
 
-#path2=os.path.join(path1,'Output_files/Multi_CARSEC'))
+path2=os.path.join(path1,'Output_files/Multi_CARSEC')
+
+st.write(path2)
 		
 
 if uploaded_file is not None:
