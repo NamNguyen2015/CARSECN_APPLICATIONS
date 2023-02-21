48941890f0ba9433fd1079672cbf9f40605d18a0
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-02-21 12:37:05 +0100

    Update 2_Single_input_(Example).py

--- pages/2_Single_input_(Example).py
+++ pages/2_Single_input_(Example).py
@@ -168,14 +168,15 @@ DB['contorno']= collect_numbers(numbers)
 
 # *************************
 # hp
-#st.write("\n")
-#st.write("-***“hp”*** puntos que definen el contorno poligonal")
-#st.markdown('**Contorno Poligonal**')
+st.write("\n")
+st.write("-***“hp”*** puntos que definen el contorno poligonal")
+st.markdown('**Contorno Poligonal**')
 
-#st.markdown('**hp**')
-#collect_numbers = lambda x : [{'Punto_'+str(int(i)):int(i) for i in re.split("[^0-9]", x) if i != "" }]
-#numbers = st.text_input("Enter la lista de los puntos hp:")
-#DB['hp']= collect_numbers(numbers)
+st.markdown('**hp**')
+
+collect_numbers_hp = lambda x : [{'Punto_'+str(int(i)):int(i) for i in re.split("[^0-9]", x) if i != "" }]
+numbers_hp = st.text_input("Enter la lista de los puntos hp:")
+DB['hp']= collect_numbers_hp(numbers_hp)
 
 
 
