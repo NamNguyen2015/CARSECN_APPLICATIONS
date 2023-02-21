7ae6ce82ac340e8692c177c1169b895bb0e7f6cb
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-02-21 11:49:26 +0100

    Update CARSEC.py

--- CARSEC.py
+++ CARSEC.py
@@ -35,7 +35,7 @@ import copy
 # DB['horm']=float
 # DB['contorno']=[0:{'Punto_1':1,'Punto_2':2,'Punto_3':3,'Punto_4':4,...}]
 
-# DB['hp']=[0:{'Punto_Central':5,'Radio':0.3}]
+# DB['hp']=0:{'Punto_1':1,'Punto_2':2,'Punto_3':3,'Punto_4':4,...}]
 #********
 # DB['hc']=[0:{'Punto_Central':5,'Radio':0.3}]
 #********
@@ -86,14 +86,9 @@ def CARSEC_Writer(DB,export_path='CARSEC'):
             f.write('\n')
 	
 	
-	for v in DB['hp']:
-            for k in v.keys():           
-                f.write(str(v[k])+' ')
-            f.write('\n')
-	
 	#for v in DB['hp']:
-         #   for k in v.keys():           
-          #      f.write(str(v[k])+' ')
+            #for k in v.keys():           
+            #    f.write(str(v[k])+' ')
            # f.write('\n')
               
         f.write('hc ') 
