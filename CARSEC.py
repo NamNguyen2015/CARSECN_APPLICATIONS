a6a9f4c9338b7abb74abe13f26dcc3db94b73993
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-02-23 16:58:24 +0100

    Update CARSEC.py

--- CARSEC.py
+++ CARSEC.py
@@ -130,7 +130,7 @@ def CARSEC_Writer(DB,export_path='CARSEC'):
             for k in v.keys():
                 f.write(str(v[k])+' ' )
             f.write('\n')
-	f.write()
+	#f.write('Axil')
         f.write('fin')
 #       f.write('hc ') 
 #       for v in DB['hc'] :
